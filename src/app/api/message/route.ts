a5eda9ff022d51ecbe7772dbff02a1b681e7c18e
gaurangkhera5188 <khera.gaurang@gmail.com>
2023-10-13 22:30:32 +0530

    m

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -71,6 +71,7 @@ export const POST = async (req: NextRequest) => {
       model: 'gpt-3.5-turbo',
       temperature: 0.7, // Adjust the temperature as needed
       stream: true,
+      max_tokens: 1024,
       messages: [
         {
           role: 'system',
